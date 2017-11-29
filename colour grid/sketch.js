95eba4f03eb89b29c75145af1222dd76c391e9ac
nickbarker97 <33899541+nickbarker97@users.noreply.github.com>
2017-11-29 14:15:33 +0000

    Add files via upload

---
+++ colour grid/sketch.js
@@ -0,0 +1,30 @@
+//https://github.com/stavrosdidakis/DAT-GAD-405_2017
+//https://p5js.org
+
+//DAT405 / GAD405
+//03_NestedForLoop - Visual
+
+let size = 50;
+
+function setup() {
+  createCanvas(500, 500);
+  noLoop();
+}
+
+function draw() {
+
+
+  //Run the nested loop for x, and y
+  for (let y = 0; y < 10; y++) {
+    for (let x = 0; x < 10; x++) {
+      //Draw the shape at the corresponding position
+      //This will generate a dynamic position for the x and y
+      //The shape is redrawn as many times as the loop says (in this case 100)
+      fill(215, 200, 170);
+      rect(size * x, size * y, size, size);
+
+      fill(random(255),random(255),random(255));
+      rect (size * x + 12.5,size * y + 12.5, 25, 25);
+    }
+  }
+}
